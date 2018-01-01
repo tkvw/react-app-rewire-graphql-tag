2469a85feb43b2e5d5a9912660f84d8b11b59a6d
Sunny Wong <draco@users.noreply.github.com>
2018-01-01 20:09:21 +0800

    Guard against users not using file-loader
    If the consumer isn't using `file-loader`, don't apply the exclusion.

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ function rewireGraphQLTag(config, env) {
     const gqlExtension = /\.(graphql|gql)$/
 
     const fileLoader = config.module.rules.find(rule=> rule.loader && rule.loader.indexOf("file-loader")!==-1);
-    fileLoader.exclude.push(gqlExtension);
-
+    
+    if (fileLoader !== undefined) {
+        fileLoader.exclude.push(gqlExtension);
+    }
+   
     const gqlTagRule = {
         test: gqlExtension,
         loader: 'graphql-tag/loader',
