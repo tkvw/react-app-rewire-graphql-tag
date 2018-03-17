44a0b5ba98ae3cdac0f42326e28bbfeff0b90769
Ayc0 <benjamin.koltes@gmail.com>
2018-03-17 16:59:22 +0100

    fix: undefined exclude

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
+const gqlExtension = /\.(graphql|gql)$/;
+
+const flatten = array =>
+  array.reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []);
 
 function rewireGraphQLTag(config, env) {
-    const gqlExtension = /\.(graphql|gql)$/
+  const fileLoader = flatten(
+    config.module.rules.map(rule => rule.oneOf || rule)
+  ).find(rule => rule.loader && rule.loader.indexOf("file-loader") !== -1);
 
-    const flatten = (array) => array.reduce((a, b) =>
-        a.concat(Array.isArray(b) ? flatten(b) : b), []);
-
-    const fileLoader = flatten(config.module.rules.map((rule) => rule.oneOf || rule))
-        .find((rule) => rule.loader && rule.loader.indexOf("file-loader")!==-1);
-
-    fileLoader && fileLoader.exclude.push(gqlExtension);
-
-    const gqlTagRule = {
-        test: gqlExtension,
-        loader: 'graphql-tag/loader',
-        exclude: /node_modules/
+  if (fileLoader) {
+    if (!fileLoader.exclude) {
+      fileLoader.exclude = [gqlExtension];
+    } else {
+      fileLoader.exclude.push(gqlExtension);
     }
-    config.module.rules.push(gqlTagRule);
+  }
 
-    return config;
+  const gqlTagRule = {
+    test: gqlExtension,
+    loader: "graphql-tag/loader",
+    exclude: /node_modules/
+  };
+  config.module.rules.push(gqlTagRule);
+
+  return config;
 }
 
 module.exports = rewireGraphQLTag;
